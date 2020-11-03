6b8c8dfbf09a026ed851770e42117a2698156f3f
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-11-03 15:34:06 +0100

    mise en place du projet

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,12 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/global.scss';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
