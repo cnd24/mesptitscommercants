f6f0eb6f992c8e49fe8792d4ad6ecf93f99403a2
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-11-06 16:10:13 +0100

    mise en place du front

--- assets/app.js
+++ assets/app.js
@@ -15,5 +15,3 @@ require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
