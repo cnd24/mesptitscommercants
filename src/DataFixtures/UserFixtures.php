5691e45b3be81df9da739001aacb5fa97e2b1499
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-12-09 21:25:58 +0100

    definition des roles et fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -23,7 +23,20 @@ class UserFixtures extends Fixture
 
         $user = new User();
         $user->setEmail('admin@admin.fr');
+        $user->setRoles(['ROLE_ADMIN']);
         $user->setPassword($this->passwordEncoder->encodePassword($user, 'admin'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('shop@admin.fr');
+        $user->setRoles(['ROLE_SHOP']);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'shop'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('customer@admin.fr');
+        $user->setRoles(['ROLE_CUSTOMER']);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'customer'));
         $manager->persist($user);
 
         $manager->flush();
